a503327de1ce4eeca107b1c01c2352696d0a3e84
Habiba Hilal <121563439+habibahilal@users.noreply.github.com>
2025-05-13 21:00:27 +0300

    removing small syntax error

--- UserService/src/main/java/com/example/anghamna/UserService/Services/UserService.java
+++ UserService/src/main/java/com/example/anghamna/UserService/Services/UserService.java
@@ -1,6 +1,5 @@
 package com.example.anghamna.UserService.Services;
 import com.example.anghamna.UserService.DTOs.RegisterRequest;
-import com.example.anghamna.UserService.DTOs.UserResponse;
 import com.example.anghamna.UserService.Events.EventPublisher;
 import com.example.anghamna.UserService.Models.User;
 import com.example.anghamna.UserService.Models.UserType;
