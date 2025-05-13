2dcd3a7ec7a9ecb30a612bcad98e4daeaee94705
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2025-05-13 16:43:49 +0300

    small comment

--- UserService/src/main/java/com/example/anghamna/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/example/anghamna/UserService/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 @EnableCaching
 public class UserServiceApplication {
 
