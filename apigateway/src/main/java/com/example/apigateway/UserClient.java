207385a5adc946957031b0b793671e3fddb32391
Omar Mohammad <omarmohammadsalah12@gmail.com>
2025-05-16 19:46:49 +0300

    stream count fix

--- apigateway/src/main/java/com/example/apigateway/UserClient.java
+++ apigateway/src/main/java/com/example/apigateway/UserClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081/userapi")
+@FeignClient(name = "user-service", url = "http://user-service:8080/userapi/sessions")
 public interface UserClient {
-    @GetMapping("/session/validate")
-    boolean validateSession(@RequestParam("sessionId") String sessionId);
+    @GetMapping("/validate")
+    boolean validateSession(@RequestParam("sessionId") int sessionId);
 }
 
